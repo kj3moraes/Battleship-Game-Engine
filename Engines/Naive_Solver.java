d8e8db9fa10c41d189694eb793136b45bbc6026a
Keane Moraes <lordvader3002@gmail.com>
2021-07-03 22:27:01 +0400

    fix to remove horizontal bias for Naive-Solver

--- Engines/Naive_Solver.java
+++ Engines/Naive_Solver.java
@@ -9,38 +9,46 @@ public class Naive_Solver extends BattleshipEngine {
         char rowStart = startingPosition.charAt(0);
         int colStart = startingPosition.charAt(0);
 
-        System.out.println("Starting Position: \t " + startingPosition);
-        System.out.println("Ship : " + s.getShipName() + " \t " + s.getShipLength());
+        int horizontalOrVertical = (int) Math.round(Math.random());
 
-        // CAN IT FIT HORIZONTALLY ?
-        if (colStart - s.getShipLength() + 1 > 0) {
-            if (arena.isCorrectCoordinates(rowStart, rowStart, colStart, colStart - s.getShipLength() + 1, s)) {
-                for (int i = colStart - s.getShipLength() + 1; i <= colStart; i++) {
-                    arena.placePiece(rowStart, i, arena.SHIP);
+        // HORIZONTAL OR VERTICAL PLACEMENT VIABILITY
+        if (horizontalOrVertical == 0) {
+            // CAN IT FIT HORIZONTALLY ?
+            if (colStart - s.getShipLength() + 1 >= 1) {
+                if (arena.isCorrectCoordinates(rowStart, rowStart, colStart, colStart - s.getShipLength() + 1, s)) {
+                    for (int i = colStart - s.getShipLength() + 1; i <= colStart; i++) {
+                        arena.placePiece(rowStart, i, arena.SHIP);
+                    }
+                    return;
+                }
+            } else if (colStart + s.getShipLength() - 1 <= 10) {
+                if (arena.isCorrectCoordinates(rowStart, rowStart, colStart, colStart - s.getShipLength() + 1, s)) {
+                    for (int i = colStart; i <= colStart + s.getShipLength() - 1; i++) {
+                        arena.placePiece(rowStart, i, arena.SHIP);
+                    }
+                    return;
                 }
             }
-        } else if (colStart + s.getShipLength() - 1 <= 10) {
-            if (arena.isCorrectCoordinates(rowStart, rowStart, colStart, colStart - s.getShipLength() + 1, s)) {
-                for (int i = colStart; i <= colStart + s.getShipLength() - 1; i++) {
-                    arena.placePiece(rowStart, i, arena.SHIP);
+            placeShip(s);
+        } else {
+            // CAN IT FIT VERTICALLY ?
+            if (rowStart - s.getShipLength() + 1 >= 'A') {
+                if (arena.isCorrectCoordinates((char) (rowStart - s.getShipLength() + 1), rowStart, colStart, colStart, s)) {
+                    for (int i = colStart - s.getShipLength() + 1; i <= colStart; i++) {
+                        arena.placePiece(rowStart, i, arena.SHIP);
+                    }
+                    return;
                 }
-            }
-        }
+            } else if (rowStart + s.getShipLength() - 1 <= 'J') {
+                if (arena.isCorrectCoordinates(rowStart, (char) (rowStart + s.getShipLength() - 1), colStart, colStart - s.getShipLength() + 1, s)) {
+                    for (int i = colStart - s.getShipLength() + 1; i <= colStart; i++) {
+                        arena.placePiece(rowStart, i, arena.SHIP);
+                    }
+                    return;
+                }
 
-        // CAN IT FIT VERTICALLY ?
-        if (rowStart - s.getShipLength() > 'A') {
-            if (arena.isCorrectCoordinates(rowStart, rowStart, colStart, colStart - s.getShipLength() + 1, s)) {
-                for (int i = colStart - s.getShipLength() + 1; i <= colStart; i++) {
-                    arena.placePiece(rowStart, i, arena.SHIP);
-                }
             }
-        } else if (rowStart + s.getShipLength() < 'J') {
-            if (arena.isCorrectCoordinates(rowStart, rowStart, colStart, colStart - s.getShipLength() + 1, s)) {
-                for (int i = colStart - s.getShipLength() + 1; i <= colStart; i++) {
-                    arena.placePiece(rowStart, i, arena.SHIP);
-                }
-            }
-
+            placeShip(s);
         }
     }
 
