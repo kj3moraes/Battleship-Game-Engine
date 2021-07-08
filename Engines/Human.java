f57dddb9a15c84c386a6eaaca90f404c07efbe6f
Keane Moraes <lordvader3002@gmail.com>
2021-07-08 23:19:41 +0400

    code cleanup and deletions

--- Engines/Human.java
+++ Engines/Human.java
@@ -66,49 +66,4 @@ public class Human extends Player {
 
         return firingPos;
     }
-
-
-/*if (arena.isHit(rowCoord, columnCoord) ) {
-                arena.placePiece(rowCoord, columnCoord, arena.HIT);
-                if (arena.isSunken(rowCoord, columnCoord)) {
-                    System.out.println("You sank a ship!");
-                } else {
-                    System.out.println("You hit a ship");
-                }
-            } else if (arena.isMiss(rowCoord, columnCoord)) {
-                arena.placePiece(rowCoord, columnCoord, arena.MISS);
-                System.out.println("You missed! Try again next turn");
-            }
-            break;*/
-    /*
-     protected static void firingASalvo(Battlefield bf) {
-        Scanner num = new Scanner(System.in);
-        while (true) {
-            String firingPos = num.next().toUpperCase().trim();
-
-            char rowCoordinate = firingPos.charAt(0);
-            int columnCoordinate = Integer.parseInt(firingPos.substring(1));
-
-            if (!bf.isCorrectCoordinates(rowCoordinate, 'A', columnCoordinate, 9, null)) {
-                System.out.println("Error! You entered the wrong coordinates! Try again:");
-                continue;
-            }
-
-            char status = bf.salvoStatus(rowCoordinate - 65, columnCoordinate - 1);
-            if (status == bf.SHIP || status == bf.HIT) {
-                bf.placeResultOfSalvo(rowCoordinate - 65, columnCoordinate - 1, bf.HIT);
-                bf.printBattlefield(true);
-                if (isSunken(rowCoordinate, columnCoordinate, bf))
-                    System.out.println("You sank a ship!");
-                else
-                    System.out.println("You hit a ship! ");
-            } else if (status == bf.WATER || status == bf.MISS) {
-                bf.placeResultOfSalvo(rowCoordinate - 65, columnCoordinate - 1, bf.MISS);
-                bf.printBattlefield(true);
-                System.out.println("You missed!");
-            }
-            break;
-        }
-    }
-     */
 }//end of class
