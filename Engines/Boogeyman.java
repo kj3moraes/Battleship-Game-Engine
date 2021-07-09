c40ce6ab61638a074466fbc4b369830e9ac1fb68
Keane Moraes <lordvader3002@gmail.com>
2021-07-09 20:39:35 +0400

    initial unfinished structure to IS

---
+++ Engines/Boogeyman.java
@@ -0,0 +1,17 @@
+package Engines;
+
+import Services.Ship;
+
+public class Boogeyman extends BattleshipEngine{
+
+    @Override
+    public void placeShip(Ship s) {
+
+    }
+
+    @Override
+    public String fireASalvo() {
+
+        return null;
+    }
+}//end of class
--- Engines/IntermediateAdversary.java
+++ Engines/IntermediateAdversary.java
@@ -1,7 +1,6 @@
 package Engines;
 
 import Services.Battlefield;
-
 import java.util.List;
 import java.util.Random;
 import java.util.Stack;
@@ -25,10 +24,55 @@ public class IntermediateAdversary extends NaiveSolver {
     /**
      * createTargets(List<Integer>, List<Integer>) --------------------------------------
      * Produces a target list and a hunting list.
-     * @param targetList - parameter to create a list
-     * @param huntList - parameter to
+     * @param targets - parameter to create a list
+     * @param hunts - parameter to
      */
-    private void createTargets(List<Integer> targetList, List<Integer> huntList) {
+    private void createTargets(List<Integer> targets, List<Integer> hunts) {
 
     }
+
+    private String hunt() {
+        String target = "";
+        final String alphabet = "ABCDEFGHIJ";
+        int temp;
+        int row=0;
+        int col=0;
+        int index = 0;
+        list.empty(); //empty Target Mode stack because we are now in Hunt Mode
+
+        //check to make sure hunts still has valid targets
+        if(!hunts.isEmpty()){
+            index = rng.nextInt(hunts.size()); //pick random target from hunts
+            temp = hunts.get(index); //assign to temp
+
+            //these following two lines of code are for encoding integers to battleship coordinates ("A1", "B2")
+
+            row = (int) Math.floor(((float)temp)/10); //get 10^1 digit
+            col = temp%10; //get 10^0 digit
+            target = Character.toString(alphabet.charAt(col)) + Integer.toString(row+1); //coloumns are letters, rows are integers which are not 0 indexed
+
+            //System.out.println("Target = " + target);
+
+            //Remove target from both lists because it is no longe valid
+            hunts.remove(temp);
+            targets.remove(temp);
+        }
+        //if hunts is empty, pick a target from targets instead which holds remaining valid targets
+        else if(hunts.isEmpty()){
+            //same process as above
+            index = rng.nextInt(targets.size());
+            temp = targets.get(index);
+            targets.remove((Object) temp);
+            row = (int) Math.floor(((float)temp)/10);
+            col = temp%10;
+            target = Character.toString(alphabet.charAt(col)) + Integer.toString(row+1);
+        }
+        return target;
+    }
+
+    //returns true if Target Mode stack is empty
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
 }//end of class
